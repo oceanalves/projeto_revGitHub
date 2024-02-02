cfe467077a9632244e1ff8a9af685f790c5cea8f
oceanalves <occeans@gmail.com>
2024-02-02 11:17:52 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de dados")
+
+# variáveis
+idade = 55
+nome = "Ocean"
+
+# print
+print(idade)
+
+# soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# listas
+lista = [1, 2, 3, 4, 5]
+print(lista)
+
+# métodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
